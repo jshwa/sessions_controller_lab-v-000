be75bad3b57e17e67ac7ca9fc4bff6c2c9b9ed9b
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-22 02:00:07 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create 
+    if params[:name] == nil || params[:name] == ""
+      redirect_to 'new'
+    else session[:name] = params[:name]
+      redirect_to 
+    end
+  end
+
+  def destroy
+    session.destroy
+  end
 end
